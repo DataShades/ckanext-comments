4831da48f3dea83fa5c7896e131a7b6ce2a5dddd
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-05-17 09:06:18 +0300

    Avoid recursion in moderator check

--- ckanext/comments/utils.py
+++ ckanext/comments/utils.py
@@ -13,5 +13,5 @@ def is_moderator(user: model.User, comment, thread) -> bool:
     checker = tk.config.get(
         const.CONFIG_MODERATOR_CHECKER, const.DEFAULT_MODERATOR_CHECKER
     )
-    func = import_string(checker, silent=True) or is_moderator
+    func = import_string(checker, silent=True) or comments_is_moderator
     return func(user, comment, thread)
